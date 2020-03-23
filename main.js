809d5625a6f01d61fcbe886eb7b6fe3c8a0b5bda
Eva Rozsa <Eva Rozsa>
2020-03-23 16:36:31 -0600

    a-i

---
+++ main.js
@@ -0,0 +1,145 @@
+class Anteater {
+  constructor(
+    name = 'Anny',
+    color = 'brown',
+    gender = 'female',
+    weight = 150,
+    food = 'ants',
+  ) {
+    this.name = name;
+    this.color = color;
+    this.gender = gender;
+    this.weight = weight;
+    this.food = food;
+  }
+}
+export default Anteater;
+let Anny = new Anteater("Anny", "brown", "female", 150, "ants")
+
+class Barracuda {
+  constructor(
+    name = 'Barney',
+    color = 'silver',
+    gender = 'male',
+    weight = 53,
+    food = 'small tuna',
+  ) {
+    this.name = name;
+    this.color = color;
+    this.gender = gender;
+    this.weight = weight;
+    this.food = food;
+  }
+}
+export default Barracuda;
+let Barney = new Barracuda("barney", "silver", "male", 53, "small tuna")
+
+class Cobra {
+  constructor(
+    name = 'Conny',
+    color = 'black',
+    gender = 'female',
+    weight = 13,
+    food = 'birds',
+  ) {
+    this.name = name;
+    this.color = color;
+    this.gender = gender;
+    this.weight = weight;
+    this.food = food;
+  }
+}
+export default Cobra;
+let Conny = new Cobra("Conny", "black", "female", 13, "birds")
+
+class Dingo {
+  constructor(
+    name = 'Dillon',
+    color = 'sandy',
+    gender = 'male',
+    weight = 30,
+    food = 'rabbits',
+  ) {
+    this.name = name;
+    this.color = color;
+    this.gender = gender;
+    this.weight = weight;
+    this.food = food;
+  }
+}
+export default Dingo;
+let Dillon = new Dingo("Dillon", "sandy", "male", 30, "rabbits")
+
+class Echidna {
+  constructor(
+    name = 'Itchy',
+    color = 'brownSpikes',
+    gender = 'male',
+    weight = 7,
+    food = 'termites',
+  ) {
+    this.name = name;
+    this.color = color;
+    this.gender = gender;
+    this.weight = weight;
+    this.food = food;
+  }
+}
+export default Echidna;
+let Itchy = new Echidna("Itchy", "brownSpikes", "male", 7, "termites")
+
+class Flamingo {
+  constructor(
+    name = 'Fanny',
+    color = 'pink',
+    gender = 'female',
+    weight = 6,
+    food = 'shrimp',
+  ) {
+    this.name = name;
+    this.color = color;
+    this.gender = gender;
+    this.weight = weight;
+    this.food = food;
+  }
+}
+export default Flamingo;
+let Fanny = new Flamingo("Fanny", "pink", "female", 6, "shrimp")
+
+class Gorilla {
+  constructor(
+    name = 'Gregory',
+    color = 'silverBack',
+    gender = 'male',
+    weight = 350,
+    food = 'fruit',
+  ) {
+    this.name = name;
+    this.color = color;
+    this.gender = gender;
+    this.weight = weight;
+    this.food = food;
+  }
+}
+export default Gorilla;
+let Gregory = new Gorilla("Gregory", "silverBack", "male", 350, "fruit")
+
+class Horse {
+  constructor(
+    name = 'mrEd',
+    color = 'paint',
+    gender = 'male',
+    weight = 1000,
+    food = 'hay',
+  ) {
+    this.name = name;
+    this.color = color;
+    this.gender = gender;
+    this.weight = weight;
+    this.food = food;
+  }
+}
+export default Horse;
+let mrEd = new Horse("mrEd", "paint", "male", 1000, "hay")
+
+class Iguana
